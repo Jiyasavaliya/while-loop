4965bbf76ba10de633f0d063abe4d194d9d0743d
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-03 15:57:22 +0530

    Add files via upload

---
+++ n to 1 using .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, end;
+
+    printf("Print all natural numbers from 1 to : ");
+    scanf("%d", &i);
+
+    
+    while(i>=end)
+    {
+        printf("%d\n", i);
+        i--;
+    }
+
+}
